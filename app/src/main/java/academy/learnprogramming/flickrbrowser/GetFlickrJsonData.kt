654fd09074b74afde578ab4b1955cce53c477d8f
Rossi <karovarossi@gmail.com>
2022-11-15 10:35:59 +0000

    139.1

--- app/src/main/java/academy/learnprogramming/flickrbrowser/GetFlickrJsonData.kt
+++ app/src/main/java/academy/learnprogramming/flickrbrowser/GetFlickrJsonData.kt
@@ -1,13 +1,40 @@
 package academy.learnprogramming.flickrbrowser
 
 import android.os.AsyncTask
+import android.util.Log
+import org.json.JSONObject
 
 class GetFlickrJsonData (private val listener: OnDAtaAvailable) : AsyncTask<String, Void, ArrayList<Photo>>() {
 
     private val TAG = "GetFlickrJsonData"
 
     interface OnDAtaAvailable{
-        fun omDataAvailable (data: List<Photo>)
+        fun onDataAvailable (data: List<Photo>)
         fun onError(exception: Exception)
+    }
+
+    override fun doInBackground(vararg params: String?): ArrayList<Photo> {
+        Log.d (TAG, "doInBackground starts")
+        try {
+            val jsonData = JSONObject(params[0])
+            val itemsArray = jsonData.getJSONArray("items")
+
+            for (i in 0 until itemsArray.length()){
+                val jsonPhoto = itemsArray.getJSONObject(i)
+                val title = jsonPhoto.getString("title")
+                val author = jsonPhoto.getString("author")
+                val authorId = jsonPhoto.getString("author_id")
+                val tags = jsonPhoto.getString("tags")
+
+                val jsonMedia = jsonPhoto.getJSONObject("media")
+                val photoUrl = jsonMedia.getString("m")
+                val link = photoUrl.replaceFirst("_m.jpg", "_b.jpg")
+            }
+        }
+    }
+
+    override fun onPostExecute(result: ArrayList<Photo>?) {
+        Log.d (TAG, "onPostExecute starts")
+        super.onPostExecute(result)
     }
 }
