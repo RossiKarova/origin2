aee4c09e6aca69f64bcb4cb691d927b828752816
RossiKarova <karovarossi@gmail.com>
2023-01-11 12:13:31 +0000

    search

---
+++ app/src/main/java/academy/learnprogramming/flickrbrowser/PhotoDetailsActivity.kt
@@ -0,0 +1,31 @@
+package academy.learnprogramming.flickrbrowser
+
+import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.content_photo_details.*
+
+class PhotoDetailsActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_details)
+
+        activateToolBar(true)
+
+        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+
+//        photo_title.text = "Title" + photo.title
+//        photo_tags.text = "Tags" + photo.tags
+
+        photo_title.text = resources.getString(R.string.photo_title_text,photo.title)
+        photo_tags.text = resources.getString(R.string.photo_tags_text, photo.tags)
+        photo_author.text = photo.author
+
+
+        Picasso.with(this).load(photo.link)
+            .error(R.drawable.placeholder)
+            .placeholder(R.drawable.placeholder)
+            .into(photo_image)
+    }
+}
+
